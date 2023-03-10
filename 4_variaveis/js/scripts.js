c4b1a8f25dce7f38e8092dac851e8d4121bd3a0e
Aristaque <jeferson.aristaque@gmail.com>
2023-03-10 12:05:31 -0300

    Exercicio 4

---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,17 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+var meuPrimeiroNome = 'Jeferson';
+
+console.log('meuPrimeiroNome'); //camelCase
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
